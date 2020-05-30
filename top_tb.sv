6772acc7e4f5f5d9794fa4172199038d8ecedbf7
appledore22 <52023866+appledore22@users.noreply.github.com>
2020-05-30 16:21:41 +0530

    Update top_tb.sv

--- top_tb.sv
+++ top_tb.sv
@@ -13,6 +13,10 @@ module test();
   reg rready;
   
     bit [8:0] data_stored [$];
+  bit [8:0] data_computed [$];
+  
+  reg [9:0] data;
+
   
   top t1(clk,reset,wdata,wvalid,wready,rvalid,rdata,rready);
   
@@ -26,35 +30,50 @@ module test();
       #10;
       reset = 0;
       
-       
+                     rready = 0;
+
       fork
         begin
-          repeat(10)
+          repeat(40)
             begin
               wait(wready == 1);
               @(negedge clk);
               wvalid = 1;
-              wdata = $random;
+              wdata = $random();
+              data_stored.push_back(wdata);
+
             end
           	wvalid = 0;
         end
         begin
           #40;
-          repeat(20)
+          repeat(6)
             begin
               @(posedge clk);
               wait(rvalid == 1);
               @(negedge clk);
               rready = 1;
-              @(negedge clk);
+              @(posedge clk);
+              #3;
               rready = 0;
-              data_stored.push_back(rdata);
+              data_computed.push_back(rdata);
             end
         end
       join_none
       
       #600;
       $display("%p",data_stored);
+      $display("%p",data_computed);
+      
+      repeat(data_computed.size())
+        begin
+         	data_stored.pop(data);
+          case(data[9:8])
+            0:if()
+             
+          
+        end
+
       $finish;
     end
   
